0b4f14387298758cf8ecf1156def30dab56b95cf
akvelmurugan <akvelmurugan74@gmail.com>
2025-08-05 11:56:59 +0530

    version-2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>CICD Section S211</h1>
+      <h1>KL University, CICD Section S211</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
